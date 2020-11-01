6d0135b47f8ae8c26fb5f29e089d2b3efa53db31
gizemcdeniz <gizemc.deniz@gmail.com>
2020-11-01 13:59:10 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your functions here
+function map (array,string) {
+    return array.map(string)
+}
+
+const reduce = (array,fn,startingP = 0) => {
+    let result;
+    if (startingP > 0) {
+        result = array.reduce(fn, startingP);
+    }
+    else {
+        result = array.reduce(fn);
+    }return result;
+}
